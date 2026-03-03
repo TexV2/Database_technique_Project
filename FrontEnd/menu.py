cd568d521afdf5a15a205c21d4c73e189923f5fa
at0paz21 <keei25@student.bth.se>
2026-03-03 13:16:09 +0100

    Polished the menu and added Infrastructure menu

--- FrontEnd/menu.py
+++ FrontEnd/menu.py
@@ -5,30 +5,58 @@ from BackEnd import assignments as assignments
 from BackEnd import log as log
 from BackEnd import schema as schema
 
-def schemaMenu():
+def schema_menu():
     end = False
     while not end:
         print ("Choose: ")
-        print ("1) Drop all tables")
+        print ("1) Drop all tables") #Unnecessary?
         print ("2) Reset all tables")
         print ("3) Show all tables")
         print ("4) Go back")
-        choice = input("-->").lower().strip()
+        choice = input("--> ").lower().strip()
         match choice:
             case "1":
                 schema.dropAllTables(main.get_name(), main.get_connection())
-                print("\n\nData has successfully been dropped")
+                print("\n\nData has successfully been dropped.")
             case "2":
                 schema.dropAllTables(main.get_name(), main.get_connection())
                 main.main_setup()
-                print("\n\nData has successfully been reset")
+                print("\n\nData has successfully been reset.")
             case "3":
                 conn = main.get_connection()
                 curr = conn.cursor()
-                main.print_tables(curr, conn)
+                main.print_tables(curr)
             case "4":
-                print ("Going back to main menu ")
+                print ("Going back to main menu. ")
                 end = True
+            case _:
+                print("Invalid input, please try again.")
+
+        if not end:    
+            input("\nPress enter to continue...")
+
+def infrastructure_menu():
+    end = False
+    while not end:
+        print("Choose:")
+        print("1) Show table")
+        print("2) Get status of infrastructures")
+        print("3) Add infrastructure")
+        print("4) More information about a specific infrastructure")
+        print("5) Update specific infrastructure")
+        print("6) Go back")
+        choice = input("--> ").lower().strip()
+        match choice:
+            case "1":
+                
+            case "6":
+                print ("Going back to main menu. ")
+                end = True
+            case _:
+                print("Invalid input, please try again.")
+
+        if not end:
+            input("\nPress enter to continue...")
 
 def menu():
     end = False
@@ -40,12 +68,12 @@ def menu():
         print("4) Assignment commands ")
         print("5) Log commands")
         print("q) Quit")
-        choice = input("-->").lower().strip()
+        choice = input("--> ").lower().strip()
         match choice:
             case "1":
-                schemaMenu()
+                schema_menu()
             case "2":
-                print("Idk yet")
+                infrastructure_menu()
             case "q":
                 print("Goodbye.")
                 end = True
